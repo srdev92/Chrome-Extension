ab733684562f3b5ae995c491a6c1dc03275ed478
srdev92 <mykolagolub0920@gmail.com>
2024-01-07 02:29:50 +0000

    initial

---
+++ scripts/content.js
@@ -0,0 +1,32 @@
+/*
+// const article = document.querySelector("article");
+const article = document.querySelector("body");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+console.log(article);
+
+if (article) {
+    const text = article.textContent;
+    const wordMatchRegExp = /[^\s]+/g; // Regular expression
+    const words = text.matchAll(wordMatchRegExp);
+    // matchAll returns an iterator, convert to array to get word count
+    const wordCount = [...words].length;
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    // Use the same styling as the publish information in an article's header
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
+
+    // Support for API reference docs
+    const heading = article.querySelector("h1");
+    // Support for article docs with date
+    const date = article.querySelector("time")?.parentNode;
+
+    (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+*/
+
+async function fetchData() {
+    console.log(0.2 + 0.1);
+}
+fetchData();
